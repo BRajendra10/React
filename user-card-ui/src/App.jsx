19f7401a361436682322ec94ee9195275e2b7b6c
BRajendra10 <rajendrabehera8116@gmail.com>
2025-06-20 19:24:47 +0530

    new project with complete react + tailwind-css + react icon with vite

---
+++ user-card-ui/src/App.jsx
@@ -0,0 +1,17 @@
+import { FaBeer } from 'react-icons/fa';
+import { IoMdHeart } from "react-icons/io";
+
+import './App.css'
+import './index.css'
+
+function App() {
+  return (
+    <div className="bg-gray-50">
+      <div className="w-[100%] h-screen">
+        <IoMdHeart size={50} className="text-red-600" />
+      </div>
+    </div>
+  )
+}
+
+export default App
