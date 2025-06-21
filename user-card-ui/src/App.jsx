5c2248b07840c1ae534c40658486fae23cb3eb02
BRajendra10 <rajendrabehera8116@gmail.com>
2025-06-21 06:24:42 +0530

    user card banner and user image is complete

--- user-card-ui/src/App.jsx
+++ user-card-ui/src/App.jsx
@@ -1,14 +1,25 @@
 import { FaBeer } from 'react-icons/fa';
 import { IoMdHeart } from "react-icons/io";
+import userImage from "./assets/User-1.png";
 
-import './App.css'
 import './index.css'
 
 function App() {
   return (
-    <div className="bg-gray-50">
-      <div className="w-[100%] h-screen">
-        <IoMdHeart size={50} className="text-red-600" />
+    <div className="flex justify-center items-center w-full h-screen bg-blue-50">
+      <div className="w-[400px] h-[440px] relative bg-white shadow-2xl rounded-xl p-3">
+        {/** Header - banner **/}
+        <div className="flex justify-center items-center w-full h-[110px] bg-slate-900 rounded-xl">
+          <h1 className="text-slate-300 text-xl font-medium">Design + Code</h1>
+        </div>
+
+        {/** Header - user image**/}
+        <div className="rounded-full bg-slate-900 w-[90px] h-[90px] absolute top-[88px] left-8 border-4 border-violet-50">
+          <img src={userImage} className="w-full rounded-full" alt="user-image" />
+        </div>
+
+
+        
       </div>
     </div>
   )
