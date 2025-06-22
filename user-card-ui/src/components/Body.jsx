bf3fd75b12e2087546bbeb8e07655e0248124c53
BRajendra10 <rajendrabehera8116@gmail.com>
2025-06-22 07:18:45 +0530

    Project complete

--- user-card-ui/src/components/Body.jsx
+++ user-card-ui/src/components/Body.jsx
@@ -1,9 +1,17 @@
+import { useState } from 'react';
+
 // React icons
 import { FaBeer } from 'react-icons/fa';
 import { IoMdLink } from "react-icons/io";
 import { IoLocationOutline } from "react-icons/io5";
 
 function Body() {
+    const [followers, setFollowers] = useState(84);
+
+    function handleClick() {
+        setFollowers(followers => followers + 1);
+    }
+    
     return (
         <div className=" w-full relative top-[77px] px-3">
             <div className="flex justify-between items-center">
@@ -12,7 +20,7 @@ function Body() {
                     <h5 className="text-zinc-600">@Kartikk</h5>
                 </div>
 
-                <button className="bg-blue-500 text-zinc-50 py-2 px-7 rounded-full">Follow</button>
+                <button className="bg-blue-500 text-zinc-50 py-2 px-7 rounded-full" onClick={() => handleClick()}>Follow</button>
             </div>
 
             <p className="text-zinc-600 mt-3">Building Websites and Webapps with seamless User Experience Acroos Devices.</p>
@@ -24,13 +32,13 @@ function Body() {
                 </span>
 
                 <span className="flex items-center gap-2">
-                    <div className="text-lg font-semibold">84</div>
+                    <div className="text-lg font-semibold">{followers}</div>
                     <div className="text-lg text-zinc-600">Follwers</div>
                 </span>
             </div>
 
             <div className="mt-2">
-                <a href="#" className="flex justify-start items-center gap-2 text-blue-500 block">
+                <a href="#" className="flex justify-start items-center gap-2 text-blue-500">
                     <IoMdLink />
                     <span>kartikkhorwal.com</span>
                 </a>
