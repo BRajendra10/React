7d06b6d9536cdced299cb3c70afd57f4265be077
BRajendra10 <rajendrabehera8116@gmail.com>
2025-07-25 15:11:44 +0530

    file and folders setup complete

---
+++ Bootstrap-user-template/src/App.jsx
@@ -0,0 +1,12 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css'
+
+function App() {
+  return (
+    <div className="vh-100 vw-100 bg-primary d-flex justify-content-center align-items-center">
+      <h1 className="text-white">Full Screen</h1>
+    </div>
+  )
+}
+
+export default App
