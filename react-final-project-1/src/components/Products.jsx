bf8ee95787acd0e9dc99fc163f80b6e25a166d90
BRajendra10 <rajendrabehera8116@gmail.com>
2025-08-02 07:28:45 +0530

    layout set for filteration

--- react-final-project-1/src/components/Products.jsx
+++ react-final-project-1/src/components/Products.jsx
@@ -24,7 +24,19 @@ function Products() {
   }, [login])
 
   return (
-    <div className="min-h-screen grid grid-cols-12">
+    <div className="min-h-screen grid grid-cols-12 gap-5">
+      <div className="h-10 col-start-2 col-end-12 grid grid-cols-20">
+        <select className="col-start-18 col-end-21 text-lg bg-gray-200 rounded-lg p-2" name="filter" id="filter-product">
+          <option value="">Filter products</option>
+          <option value="low-to-high">Price: low to hight</option>
+          <option value="hight-to-low">Price: hight to low</option>
+          <option value="beauty">Category: Beauty</option>
+          <option value="fragrances">Category: Fragrances</option>
+          <option value="furniture">Category: Furniture</option>
+          <option value="groceries">Category: Groceries</option>
+        </select>
+      </div>
+
       <div className="col-start-2 col-end-12 grid grid-cols-3 gap-4">
         {
           data.map((el) => <ProductCard id={el.id} title={el.title} description={el.description} price={el.price} image={el.thumbnail} />)
