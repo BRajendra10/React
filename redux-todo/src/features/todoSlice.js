0cca02cff974912809db53fe35782c5db00ba3e5
BRajendra10 <rajendrabehera8116@gmail.com>
2025-08-05 19:54:12 +0530

    adding dynamic todo

---
+++ redux-todo/src/features/todoSlice.js
@@ -0,0 +1,21 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+const initialState = [{
+    title: "create your portfolio",
+    status: false
+}]
+
+const todoSlice = createSlice({
+    name: "todo",
+    initialState,
+    reducers: {
+        add: (state, action) => {
+            console.log(action);
+
+            state.push(action.payload);
+        }
+    }
+})
+
+export default todoSlice.reducer;
+export const { add } = todoSlice.actions;
