7cdd499fbb1b2a7e3649a940f7c72da897c531c1
BRajendra10 <rajendrabehera8116@gmail.com>
2025-08-07 17:11:03 +0530

    edit & delet

--- redux-todo/src/components/Todo.jsx
+++ redux-todo/src/components/Todo.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import { useState } from 'react'
 import { useSelector, useDispatch } from "react-redux";
 import { add, remove, update } from '../features/todoSlice'
+import { MdModeEditOutline } from "react-icons/md";
+import { MdDelete } from "react-icons/md";
 
 function Todo() {
     const dispatch = useDispatch();
@@ -17,7 +19,7 @@ function Todo() {
             <div className="w-[30rem] h-auto grid grid-cols-12 bg-gray-200 rounded-full">
                 <input type="text" className="col-start-1 col-end-10 rounded-full outline-0 py-3 px-5" value={newTodo} onChange={(e) => setNewTodo(e.target.value)} />
                 <button className="bg-orange-200 rounded-full col-start-10 col-end-13 p-3" onClick={() => {
-                    dispatch(isUpdating ? update({id, title: newTodo, status: false}) : add({ title: newTodo, status: false }))
+                    dispatch(isUpdating ? update({ id, title: newTodo, status: false }) : add({ title: newTodo, status: false }))
                     setNewTodo("");
                 }}>add todo</button>
             </div>
@@ -25,14 +27,17 @@ function Todo() {
             <ul className="w-[30rem] h-[20rem] border-2 border-orange-200 rounded-lg p-3">
                 {todos.map((el, i) => {
                     return (
-                        <li className='w-full text-lg flex items-center gap-2' key={i}>
-                            <input type="checkbox" name="delete-checkbox" id={i} onChange={() => dispatch(remove(i))} />
+                        <li className='w-full text-lg flex justify-between items-center gap-2' key={i}>
                             <span>{el.title} - {el.status ? "completed" : "incomplete"}</span>
-                            <button className="self-end py-1 px-3 rounded hover:bg-orange-200" onClick={() => {
-                                setIsUpdating(true);
-                                setId(i);
-                                setNewTodo(el.title);
-                            }}>edit</button>
+                            <div className="flex gap-2">
+                                <button className="text-xl hover:bg-stone-200 p-2 rounded-full" onClick={() => dispatch(remove(i))}><MdDelete /></button>
+                                <button className="text-xl hover:bg-stone-200 p-2 rounded-full" onClick={() => {
+                                    setIsUpdating(true);
+                                    setId(i);
+                                    setNewTodo(el.title);
+                                }}><MdModeEditOutline />
+                                </button>
+                            </div>
                         </li>
                     )
                 })}
