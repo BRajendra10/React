75ba96e4acd8e61e60a392c6360a3114594d167d
BRajendra10 <rajendrabehera8116@gmail.com>
2025-08-07 16:57:23 +0530

    updating todo

--- redux-todo/src/components/Todo.jsx
+++ redux-todo/src/components/Todo.jsx
@@ -4,10 +4,11 @@ import { useSelector, useDispatch } from "react-redux";
 import { add, remove, update } from '../features/todoSlice'
 
 function Todo() {
-    const todos = useSelector((state) => state.todo);
     const dispatch = useDispatch();
+    const todos = useSelector((state) => state.todo);
     const [newTodo, setNewTodo] = useState("");
-    const [id, setId] = useState(false);
+    const [isUpdating, setIsUpdating] = useState(false);
+    const [id, setId] = useState(null);
 
     return (
         <div className="w-full h-screen flex flex-col justify-center items-center gap-4">
@@ -16,7 +17,7 @@ function Todo() {
             <div className="w-[30rem] h-auto grid grid-cols-12 bg-gray-200 rounded-full">
                 <input type="text" className="col-start-1 col-end-10 rounded-full outline-0 py-3 px-5" value={newTodo} onChange={(e) => setNewTodo(e.target.value)} />
                 <button className="bg-orange-200 rounded-full col-start-10 col-end-13 p-3" onClick={() => {
-                    dispatch(id || id == 0 ? update({ id, title: newTodo, status: false }, id) : add({ title: newTodo, status: false }))
+                    dispatch(isUpdating ? update({id, title: newTodo, status: false}) : add({ title: newTodo, status: false }))
                     setNewTodo("");
                 }}>add todo</button>
             </div>
@@ -24,12 +25,14 @@ function Todo() {
             <ul className="w-[30rem] h-[20rem] border-2 border-orange-200 rounded-lg p-3">
                 {todos.map((el, i) => {
                     return (
-                        <li className='text-lg flex items-center gap-2' key={i}>
+                        <li className='w-full text-lg flex items-center gap-2' key={i}>
                             <input type="checkbox" name="delete-checkbox" id={i} onChange={() => dispatch(remove(i))} />
-                            <span onDoubleClick={() => {
+                            <span>{el.title} - {el.status ? "completed" : "incomplete"}</span>
+                            <button className="self-end py-1 px-3 rounded hover:bg-orange-200" onClick={() => {
+                                setIsUpdating(true);
                                 setId(i);
                                 setNewTodo(el.title);
-                            }}>{el.title} - {el.status ? "completed" : "incomplete"}</span>
+                            }}>edit</button>
                         </li>
                     )
                 })}
