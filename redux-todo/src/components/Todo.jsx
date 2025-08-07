b2e0b5a29dd90862a226c2fdcd03fa31dfa908c1
BRajendra10 <rajendrabehera8116@gmail.com>
2025-08-07 08:11:32 +0530

    crud opration complete

--- redux-todo/src/components/Todo.jsx
+++ redux-todo/src/components/Todo.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import { useState } from 'react'
 import { useSelector, useDispatch } from "react-redux";
-import { add, remove } from '../features/todoSlice'
+import { add, remove, update } from '../features/todoSlice'
 
 function Todo() {
     const todos = useSelector((state) => state.todo);
     const dispatch = useDispatch();
     const [newTodo, setNewTodo] = useState("");
+    const [id, setId] = useState(false);
 
     return (
         <div className="w-full h-screen flex flex-col justify-center items-center gap-4">
@@ -15,7 +16,7 @@ function Todo() {
             <div className="w-[30rem] h-auto grid grid-cols-12 bg-gray-200 rounded-full">
                 <input type="text" className="col-start-1 col-end-10 rounded-full outline-0 py-3 px-5" value={newTodo} onChange={(e) => setNewTodo(e.target.value)} />
                 <button className="bg-orange-200 rounded-full col-start-10 col-end-13 p-3" onClick={() => {
-                    dispatch(add({ title: newTodo, status: false }))
+                    dispatch(id || id == 0 ? update({ id, title: newTodo, status: false }, id) : add({ title: newTodo, status: false }))
                     setNewTodo("");
                 }}>add todo</button>
             </div>
@@ -23,9 +24,12 @@ function Todo() {
             <ul className="w-[30rem] h-[20rem] border-2 border-orange-200 rounded-lg p-3">
                 {todos.map((el, i) => {
                     return (
-                        <li className='text-lg flex items-center gap-2' key={i}> 
-                            <input type="checkbox" name="delete-checkbox" id={i} onChange={() => dispatch(remove(i))} />  
-                            <span onDoubleClick={() => setNewTodo(el.title)}>{el.title} - {el.status ? "completed" : "incomplete"}</span> 
+                        <li className='text-lg flex items-center gap-2' key={i}>
+                            <input type="checkbox" name="delete-checkbox" id={i} onChange={() => dispatch(remove(i))} />
+                            <span onDoubleClick={() => {
+                                setId(i);
+                                setNewTodo(el.title);
+                            }}>{el.title} - {el.status ? "completed" : "incomplete"}</span>
                         </li>
                     )
                 })}
--- redux-todo/src/features/todoSlice.js
+++ redux-todo/src/features/todoSlice.js
@@ -15,9 +15,15 @@ const todoSlice = createSlice({
 
         remove: (state, action) => {
             state.splice(action.payload, 1);
+        },
+
+        update: (state, action) => {
+            const {id, title, status} = action.payload;
+            
+            state.splice(id, 1, {title, status})
         }
     }
 })
 
 export default todoSlice.reducer;
-export const { add, remove } = todoSlice.actions;
+export const { add, remove, update } = todoSlice.actions;
