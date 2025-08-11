4f4abdd096065cf7e6a1fb091b259a2b9869172e
BRajendra10 <rajendrabehera8116@gmail.com>
2025-08-11 06:57:16 +0530

    setup new project

---
+++ add-to-faviorate/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="w-full h-screen flex justify-center items-center">
+      <h1>Add to faviorate system</h1>
+    </div>
+  )
+}
+
+export default App
